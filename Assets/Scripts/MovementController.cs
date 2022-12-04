96528c4ec095a8e62c61787a4b38b4f4856a4329
Jolanta Kazmierczak <jolanta.kazmierczak99@gmail.com>
2022-12-04 13:44:29 +0100

    game map

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
